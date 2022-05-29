0003f94fe288cf14f2c241bf88c4ca6dde5dbb3b
Jared <87617002+The-Jiggy@users.noreply.github.com>
2022-05-29 16:37:14 +0100

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,6 @@
+sudo pacman -S steam discord discord-canary audacity brasero chromium cmake dolphin-emu gnome-disk-utility gparted handbrake obs-studio virtualbox pavucontrol
+sudo flatpak install flathub com.spotify.Client
+sudo flatpak install flathub org.citra_emu.citra
+sudo flatpak install flathub org.ryujinx.Ryujinx
+git clone https://aur.archlinux.org/cadmus-deb.git /tmp/cadmus
+git clone https://aur.archlinux.org/minecraft-launcher.git /tmp/minecraft
